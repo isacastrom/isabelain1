0224eeb384d6479f0c07faa028d7aac5cc002402
isacastrom <159115744+isacastrom@users.noreply.github.com>
2024-02-06 00:53:14 -0700

    Add files via upload

---
+++ html/proyecto/NetBeansProjects/Holamundo/src/holamundo/Holamundo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author PC
+ */
+public class Holamundo {
+   public static void main(String args[]) {
+        // enteros
+        byte enteroB = Byte.MAX_VALUE;
+        short enteroS = Short.MAX_VALUE;
+        int entero = Integer.MAX_VALUE;
+        long largo = Long.MAX_VALUE;
+        // decimales
+        float largestFloat = Float.MAX_VALUE;
+        double largestDouble = Double.MAX_VALUE;
+        // other primitive types
+        char aChar = 'S';
+        boolean aBoolean = true;
+
+  
+     System.out.println("Hola in1 prepa 20-30");
+     System.out.println("Hola in1 prepa 20-30");
+     System.out.println("Hola in1 prepa 20-30");
+               
+    }
+    
+}
